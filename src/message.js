f0c2bb977afb9e34aa2ae79ba51e850ca9a4b864
#云淡然 <cloudcome@163.com>
2019-07-30 01:02:19 +0800

    ~ 目录调整

---
+++ src/message.js
@@ -0,0 +1,13 @@
+/**
+ * message
+ * @author ydr.me
+ * @create 2019-07-30 01:01:09
+ * @update 2019-07-30 01:01:09
+ */
+
+
+'use strict';
+
+
+
+
--- test/test.index.js
+++ test/test.index.js
@@ -8,49 +8,10 @@
 'use strict';
 
 var expect = require('chai').expect;
-var weixin = require('../src/index.js');
-
 
 describe('测试文件', function () {
     it('base', function (done) {
-        var url = 'https://ydr.me/';
-
-        weixin.config({
-            debug: true,
-            appId: 'wxa8c289037620b15c',
-            appSecret: '397c03bae013c5ade076009c973edf2e'
-        });
-
-        weixin.jsApiSignature(url, function (err, sign) {
-            if (err) {
-                return done(err);
-            }
-
-            console.log(sign);
-            done();
-        });
-    });
-
-    it('mutiple instance', function (done) {
-        var url = 'https://ydr.me/';
-
-        weixin.config({
-            debug: true,
-            appId: 'a',
-            appSecret: 'b'
-        });
-
-        weixin.jsApiSignature({
-            appId: 'wxa8c289037620b15c',
-            appSecret: '397c03bae013c5ade076009c973edf2e'
-        }, url, function (err, sign) {
-            if (err) {
-                return done(err);
-            }
-
-            console.log(sign);
-            done();
-        });
+        // empty
     });
 });
 
